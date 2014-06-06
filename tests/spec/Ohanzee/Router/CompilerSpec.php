2767af6932fd072b8b4e6d644436b5447d1e511e
Robert-Jan de Dreu <robert-jan@affiliate4you.nl>
2014-06-06 16:32:45 +0200

    Added Test for the route compiler

--- tests/spec/Ohanzee/Router/CompilerSpec.php
+++ tests/spec/Ohanzee/Router/CompilerSpec.php
@@ -11,4 +11,31 @@ class CompilerSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Ohanzee\Router\Compiler');
     }
+
+	function it_converts_uri_strings_into_matching_regexes()
+	{
+		$this->compile('foo/<bar>')->shouldMatchUri('foo/bar');
+	}
+
+	function it_changes_the_variable_scope_based_on_regex()
+	{
+		$this->compile('foo/<bar>', array('bar' => '\d'))->shouldNotMatchUri('foo/bar');
+	}
+
+	function it_makes_parts_of_the_uri_optional()
+	{
+		$this->compile('foo/<bar>(/<foo>)')->shouldMatchUri('foo/bar');
+	}
+
+	public function getMatchers()
+	{
+		return [
+			'matchUri' => function($subject, $uri) {
+				return preg_match($subject, $uri);
+			},
+			'notMatchUri' => function($subject, $uri) {
+				return (preg_match($subject, $uri) === 0);
+			},
+		];
+	}
 }
